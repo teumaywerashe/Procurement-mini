84b42cd6a5eab79d76d3e452533eced2b2d8ea07
teumaywerashe <teumay21@gmail.com>
2026-07-23 18:53:51 +0300

    initial commit

---
+++ backend/src/main.ts
@@ -0,0 +1,10 @@
+import * as dotenv from 'dotenv';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+dotenv.config();
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  console.log(process.env.PORT);
+  await app.listen(process.env.PORT ?? 3000);
+}
+bootstrap();
